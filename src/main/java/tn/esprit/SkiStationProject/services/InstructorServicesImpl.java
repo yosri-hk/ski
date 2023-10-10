ee7c58ba50b1c6bcf50dea5cce12993f4fe6e2d0
yosri-hk <yosri.hajkhlifa@esprit.tn>
2023-10-11 00:42:29 +0100

    first commit

---
+++ src/main/java/tn/esprit/SkiStationProject/services/InstructorServicesImpl.java
@@ -0,0 +1,51 @@
+package tn.esprit.SkiStationProject.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import tn.esprit.SkiStationProject.entities.Course;
+import tn.esprit.SkiStationProject.entities.Instructor;
+import tn.esprit.SkiStationProject.repositories.CourseRepository;
+import tn.esprit.SkiStationProject.repositories.InstructorRepository;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Service
+public class InstructorServicesImpl implements IInstructorServices{
+
+    private final InstructorRepository instructorRepository;
+    private final CourseRepository courseRepository;
+
+    @Override
+    public Instructor addInstructor(Instructor instructor) {
+        return instructorRepository.save(instructor);
+    }
+
+    @Override
+    public List<Instructor> retrieveAllInstructors() {
+        return instructorRepository.findAll();
+    }
+
+    @Override
+    public Instructor updateInstructor(Instructor instructor) {
+        return instructorRepository.save(instructor);
+    }
+
+    @Override
+    public Instructor retrieveInstructor(Long numInstructor) {
+        return instructorRepository.findById(numInstructor).orElse(null);
+    }
+
+    @Override
+    public Instructor addInstructorAndAssignToCourse(Instructor instructor, Long numCourse) {
+        Course course = courseRepository.findById(numCourse).orElse(null);
+        Set<Course> courseSet = new HashSet<>();
+        courseSet.add(course);
+        instructor.setCourses(courseSet);
+        return instructorRepository.save(instructor);
+    }
+
+
+}
